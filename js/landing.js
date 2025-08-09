c556c5d523c6ebed134dfaf71544ac896d32dfb1
Axel Wing <axelpwm@gmail.com>
2025-08-09 13:30:24 -0600

    v2

--- js/landing.js
+++ js/landing.js
@@ -7,3 +7,19 @@ document.addEventListener("DOMContentLoaded", function () {
         navbarCollapse.classList.toggle('show');
     });
 });
+// Botón Scroll Up
+const scrollBtn = document.getElementById("scrollTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollBtn.style.display = "block";
+    scrollBtn.style.opacity = "1";
+  } else {
+    scrollBtn.style.opacity = "0";
+    setTimeout(() => (scrollBtn.style.display = "none"), 300);
+  }
+});
+
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
