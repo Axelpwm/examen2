576fbf24bd8773ca8f435ab25fbfed33b532fb48
Axel Wing <axelpwm@gmail.com>
2025-08-08 23:16:34 -0600

    v1

---
+++ js/landing.js
@@ -0,0 +1,9 @@
+// scripts.js
+document.addEventListener("DOMContentLoaded", function () {
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const navbarCollapse = document.querySelector('#navbarNav');
+
+    navbarToggler.addEventListener('click', function () {
+        navbarCollapse.classList.toggle('show');
+    });
+});
